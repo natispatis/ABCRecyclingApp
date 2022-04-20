3f0310cd6584cb6b79b5ac6e8aedad62c26a06da
natispatis <s18444@pjwstk.edu.pl>
2022-04-20 23:21:38 +0200

    database

--- abc-recycling-app/src/App.js
+++ abc-recycling-app/src/App.js
@@ -24,3 +24,93 @@ const App = () => {
 }
 
 export default App;
+
+//connectng to database
+const express = require('express');
+const res = require('express/lib/response');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'ABCRecycling'
+});
+
+//connection
+db.connect((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log('MySQL Connected!');
+});
+
+const app = express();
+
+//create db
+app.get('/createdb', (req, res) => {
+    let sql = 'CREAE DATABASE ABCRecycling';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database created...');
+    })
+}) 
+
+//create table
+app.get('/createcartable', () => {
+    let sql = 'create table cars(carId int, registrationNumber varchar(20), overviewDate date)';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Cars created...');
+    })
+})
+
+//insert example
+app.get('/addcar', (req, res) => {
+    let car1 = {carId: 1, registrationNumber: 'WL19283'};
+    let sql = 'INSERT INTO cars SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Cars created...');
+    })
+})
+
+//select example
+app.get('/getcars/:id', (req, res) => {
+    let sql = `select * from cars where id = ${req.params.id}`;
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send('Car fetched...');
+    })
+})
+
+//update example
+app.get('/updatecar/:id', (req, res) => {
+    let newId = 'Updated';
+    let sql = `update Cars set title = '${newId}' where carId = ${req.params.id}`;
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send('Car updated...');
+    })
+})
+
+//delete example
+app.get('/deletecar/:id', (req, res) => {
+    let sql = `delete from cars where id = ${req.params.id}`;
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send('Car deleted...');
+    })
+})
+
+app.listen('3306', () => {
+    console.log('Server started on port 3306');
+});
+
+
